ef594c4ab7f3bc08f746faf622ba66291f8eaf82
saolsen <steve@steve.computer>
2023-03-27 11:26:01 -0500

    python version working, slow

--- tests/test_connect4.py
+++ tests/test_connect4.py
@@ -1,4 +1,5 @@
 from connect4 import State, Player
+from connect4.agent import rand_agent, mcts_agent
 
 
 def test_board_check():
@@ -24,3 +25,15 @@ def test_board_check():
     result = board.turn(Player.Red, 0)
 
     assert result == Player.Red
+
+
+# NOTE: This takes a long time because my code is all slow still.
+def test_vs_random():
+    """
+    If the mcts bot is any good than surely it wont ever lose to the random bot
+    """
+    for game in range(0, 10):
+        state = State()
+        result, winner = state.play(mcts_agent, rand_agent)
+        if result == "WIN":
+            assert winner == Player.Blue
