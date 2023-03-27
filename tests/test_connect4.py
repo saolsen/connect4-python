310eeeed2638e58f2b060bb92fb918c383486b73
saolsen <steve@steve.computer>
2023-03-27 11:29:32 -0500

    test

--- tests/test_connect4.py
+++ tests/test_connect4.py
@@ -2,31 +2,6 @@ from connect4 import State, Player
 from connect4.agent import rand_agent, mcts_agent
 
 
-def test_board_check():
-    board = State()
-    board.turn(Player.Blue, 2)
-    board.display()
-    board.turn(Player.Red, 3)
-    board.display()
-    board.turn(Player.Blue, 1)
-    board.display()
-    board.turn(Player.Red, 2)
-    board.display()
-    board.turn(Player.Blue, 0)
-    board.display()
-    board.turn(Player.Red, 0)
-    board.display()
-    board.turn(Player.Blue, 1)
-    board.display()
-    board.turn(Player.Red, 1)
-    board.display()
-    board.turn(Player.Blue, 0)
-    board.display()
-    result = board.turn(Player.Red, 0)
-
-    assert result == Player.Red
-
-
 # NOTE: This takes a long time because my code is all slow still.
 def test_vs_random():
     """
